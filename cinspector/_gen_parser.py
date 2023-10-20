0dc627a95e74a452f403509387dccd79a13d041b
Peiwei Hu <jlu.hpw@foxmail.com>
2023-10-20 09:40:50 +0800

    assign version of tree-sitter-c

--- cinspector/_gen_parser.py
+++ cinspector/_gen_parser.py
@@ -1,6 +1,8 @@
 import subprocess
 from tree_sitter import Language
 
-subprocess.check_call(
-    ['git', 'clone', 'https://github.com/tree-sitter/tree-sitter-c.git'])
+subprocess.check_call([
+    'git', 'clone', '-b', 'v0.20.2',
+    'https://github.com/tree-sitter/tree-sitter-c.git'
+])
 Language.build_library('cinspector-tree-sitter.so', ['tree-sitter-c'])
